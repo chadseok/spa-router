88c6923dcd77246f7d889836e0e423cec726f02c
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-08-21 18:36:05 +0900

    Routes, Route기능 추가
    Route를 감싸는 Routes컴포넌트와 component와 path정보를 가지고
    있는 Route컴포넌트 기능을 구현하였습니다.
    
    Routes컴포넌트에서 children의 path 정보를 forEach로 순회하면서 현재
    path와 일치하는 children컴포넌트를 반환하도록 하였습니다

--- src/router/components.tsx
+++ src/router/components.tsx
@@ -31,3 +31,30 @@ export function Router(props: RouterProps) {
     </RouterContext.Provider>
   );
 }
+
+export interface RoutesProps {
+  children: Array<React.ReactElement>;
+}
+
+export function Routes(props: RoutesProps) {
+  const { path } = React.useContext(RouterContext);
+
+  let childComponent = null;
+
+  React.Children.forEach(props.children, (child) => {
+    if (child.props.path === path) {
+      childComponent = child.props.component;
+    }
+  });
+
+  return childComponent;
+}
+
+export interface RouteProps {
+  component: React.ReactElement;
+  path: string;
+}
+
+export function Route(props: RouteProps) {
+  return props.component;
+}
