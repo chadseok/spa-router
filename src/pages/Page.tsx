b3f342228063fe5643c82900aacaa4ed82109147
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-08-24 15:58:50 +0900

    Page컴포넌트 구현 및 css적용
    Page컴포넌트에서는 useNavigate를 사용하여 button에 onClick이벤트에
    페이지를 변경하는 기능을 적용하였습니다

---
+++ src/pages/Page.tsx
@@ -0,0 +1,27 @@
+import "./Page.css";
+import { useNavigate } from "@/router";
+
+const pageList = ["A", "B", "C"] as const;
+
+type Props = {
+  name: (typeof pageList)[number];
+};
+
+const Template = (props: Props) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="wrapper">
+      <h1 className="title">Page {props.name}</h1>
+      <div>
+        {pageList.map((name) => (
+          <button className="button" onClick={() => navigate(`/${name}`)}>
+            Page {name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Template;
